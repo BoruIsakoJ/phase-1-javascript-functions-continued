a480ffc21c35f09e7169dc75f179e64d50229f30
Boru Isako <isco.jaldy@gmail.com>
2025-03-04 13:37:36 +0300

    Complete labs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+console.log(saturdayFun("swim"))
+function mondayWork(work="go to the office"){
+    return`This Monday, I will ${work}.`
+}
+function wrapAdjective(char='*'){
+    return function(adj="special"){
+      return `You are ${char}${adj}${char}!`
+      
+    } 
+}
